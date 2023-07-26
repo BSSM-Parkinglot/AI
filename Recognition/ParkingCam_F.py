c84cd38b37f90ed5b94ec5d50b59fdd300091ca7
anys34 <anyeseong34@gmail.com>
2023-07-26 21:38:19 +0900

    ADD :: 출입문 닫힘

--- Recognition/ParkingCam_F.py
+++ Recognition/ParkingCam_F.py
@@ -38,14 +38,30 @@ def save_to_database(text):
     cursor.close()
     cnx.close()
 
-def delete_to_database():
+def open_Breaker_to_database():
     # 데이터베이스와 연결 설정
     cnx = mysql.connector.connect(user='root', password='mysql', host='svc.sel4.cloudtype.app', database='parkinglot', port='32676')
     cursor = cnx.cursor()
 
     # 텍스트를 데이터베이스에 저장
-    query = "UPDATE Spot_Info SET Parking = 0, ParkingNum = NULL WHERE Spot = %s"
-    cursor.execute(query, ('A1',))
+    query = "UPDATE Breaker SET Situation = 1 WHERE `Exit` = 1"
+    cursor.execute(query, ())
+
+    # 변동 사항 커밋
+    cnx.commit()
+
+    # 연결 해제
+    cursor.close()
+    cnx.close()
+
+def close_Breaker_to_database():
+    # 데이터베이스와 연결 설정
+    cnx = mysql.connector.connect(user='root', password='mysql', host='svc.sel4.cloudtype.app', database='parkinglot', port='32676')
+    cursor = cnx.cursor()
+
+    # 텍스트를 데이터베이스에 저장
+    query = "UPDATE Breaker SET Situation = 0 WHERE `Exit` = 1"
+    cursor.execute(query, ())
 
     # 변동 사항 커밋
     cnx.commit()
@@ -62,6 +78,7 @@ def find_combinations(recognized_texts):
     return [match.replace(' ', '') for match in matches]
 
 cnt = 0
+close = 0
 
 cap = cv2.VideoCapture(0)
 
@@ -91,15 +108,21 @@ while cap.isOpened():
     if license_plate_combinations:
         for combination in license_plate_combinations:
             if is_valid_license_plate([combination]):
+                close = 0
                 print(f"---- {combination} ----")
-                save_to_database(combination)
-    elif cnt == 15:
-        print('삭제됨')
-        delete_to_database()
-        cnt = 0
+                print("출입구가 열렸습니다.")
+                open_Breaker_to_database()
+                print('자동차 정보가 저장되었습니다.')
     elif len(recognized_texts) > 0:
         print(recognized_texts)
+        close = 0
         cnt += 1
+    else:
+        close += 1
+        if (60 == close):
+            print('출입구가 닫혔습니다.')
+            close_Breaker_to_database()
+            close = 0
 
     cv2.imshow('Webcam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
